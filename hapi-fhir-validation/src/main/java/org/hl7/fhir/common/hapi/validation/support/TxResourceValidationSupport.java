31ab7768c41c4c6ed7eb9c6a09caa7ef6f6329d3
Joel Bach <6702422+joel-bach@users.noreply.github.com>
2025-11-19 10:36:40 +0100

    add tx-resource support

---
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/support/TxResourceValidationSupport.java
@@ -0,0 +1,60 @@
+package org.hl7.fhir.common.hapi.validation.support;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import jakarta.annotation.Nullable;
+import org.apache.commons.lang3.Validate;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.ValueSet;
+
+import java.util.List;
+
+public class TxResourceValidationSupport implements IValidationSupport {
+	private static final ThreadLocal<List<IBaseResource>> ourTxResourceThreadLocal = new ThreadLocal<>();
+
+	private final FhirContext myCtx;
+
+	public TxResourceValidationSupport(FhirContext theCtx) {
+		Validate.notNull(theCtx, "theCtx must not be null");
+		myCtx = theCtx;
+	}
+
+	@Override
+	public String getName() {
+		return myCtx.getVersion().getVersion() + " TxResourceValidationSupport";
+	}
+
+	@Override
+	public FhirContext getFhirContext() {
+		return myCtx;
+	}
+
+	@Override
+	public @Nullable IBaseResource fetchValueSet(String theValueSetUrl) {
+		var txResources = ourTxResourceThreadLocal.get();
+		return txResources.stream()
+				.filter(resource ->
+						resource instanceof ValueSet vs && vs.getUrl().equals(theValueSetUrl))
+				.findFirst()
+				.orElse(null);
+	}
+
+	@Override
+	public @Nullable IBaseResource fetchCodeSystem(String theSystem) {
+		var txResources = ourTxResourceThreadLocal.get();
+		return txResources.stream()
+				.filter(resource ->
+						resource instanceof CodeSystem cs && cs.getUrl().equals(theSystem))
+				.findFirst()
+				.orElse(null);
+	}
+
+	public void setTxResourceForCurrentRequest(List<IBaseResource> theTxResources) {
+		ourTxResourceThreadLocal.set(theTxResources);
+	}
+
+	public void clearTxResourceForCurrentRequest() {
+		ourTxResourceThreadLocal.remove();
+	}
+}
